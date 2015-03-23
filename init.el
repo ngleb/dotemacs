1c4d83c0d6f27f6d63b7ce3ff7745a483a777c65
Gleb Nemshilov <ngleb@users.noreply.github.com>
2015-03-24 01:59:36 +0600

    Some changes for init.el

--- init.el
+++ init.el
@@ -8,12 +8,12 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 (setq default-frame-alist
-			'(
-				(width . 120)
-				(height . 40)
-				(top . 200)
-				(left . 500)
-				))
+	  '(
+		(width . 120)
+		(height . 40)
+		(top . 200)
+		(left . 500)
+		))
 
 ;; MULE setup
 (set-language-environment 'UTF-8)
@@ -24,12 +24,16 @@
 
 (setq enable-local-variables :safe)
 (setq inhibit-startup-message t)
-(setq default-tab-width 4)
+
+;; Tab & indent setup
+;(setq-default tab-width 4)
+;(setq-default indent-tabs-mode t)
+;(setq-default tab-always-indent t)
 
 (fset 'yes-or-no-p 'y-or-n-p)
 (setq delete-old-versions nil)
 
-(setq split-width-threshold nil)
+;(setq split-width-threshold nil)
 (blink-cursor-mode -1)
 (show-paren-mode 1)
 (savehist-mode 1)
@@ -40,12 +44,11 @@
 (prefer-coding-system 'utf-8)
 
 (add-to-list 'default-frame-alist
-						 '(font . "Consolas 10"))
+			 '(font . "Consolas 10"))
 
 (setq visible-bell t)
 (setq show-paren-delay 0)
-(setq-default tab-width 2)
-(setq culumn-number-mode t)
+(setq column-number-mode t)
 (setq-default indicate-empty-lines t)
 (setq-default show-trailing-whitespace t)
 
@@ -62,22 +65,22 @@
 (global-set-key (kbd "C-c b") 'org-iswitchb)
 (global-set-key (kbd "C-c c") 'org-capture)
 (setq org-todo-keywords
-			'((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!/!)")
-				(sequence "WAITING(w@/!)" "HOLD(h@/!)" "SOMEDAY(o)" "|" "CANCELLED(c@/!)")))
+	  '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!/!)")
+		(sequence "WAITING(w@/!)" "HOLD(h@/!)" "SOMEDAY(o)" "|" "CANCELLED(c@/!)")))
 (setq org-default-notes-file (expand-file-name "~/my/org/todo.org"))
 (setq org-capture-templates
-			'(("r" "Todo" entry (file+headline "~/my/org/inbox.org" "Inbox")
-				 "* TODO %?\n %i\n %a")
-				("j" "Journal" entry (file+datetree "~/my/org/journal.org")
-				 "* %?\n entered on %U\n  %i\n  %a")))
+	  '(("r" "Todo" entry (file+headline "~/my/org/inbox.org" "Inbox")
+		 "* TODO %?\n %i\n %a")
+		("j" "Journal" entry (file+datetree "~/my/org/journal.org")
+		 "* %?\n entered on %U\n  %i\n  %a")))
 (global-set-key (kbd "C-c r")
-								(lambda () (interactive) (org-capture nil "r")))
+				(lambda () (interactive) (org-capture nil "r")))
 (global-set-key (kbd "C-c j")
-								(lambda () (interactive) (org-capture nil "j")))
+				(lambda () (interactive) (org-capture nil "j")))
 
 ; backup settings
 (setq make-backup-files t)
-(setq version-control t)
+;(setq version-control t)
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
 (setq solarized-scale-org-headlines nil)
