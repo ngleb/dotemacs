5770312fc635d6f396e01fca64ec072429c80980
Gleb Nemshilov <gleb@fastmail.com>
2017-02-03 19:55:30 +0700

    Add function for merging A and B into C

--- init.el
+++ init.el
@@ -119,6 +119,16 @@
 ;; use existing frame instead of creating a new one
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 (setq ediff-split-window-function 'split-window-horizontally)
+;; `d` for using A and B into C
+(defun ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
+
 ;; MULE & encoding setup
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
