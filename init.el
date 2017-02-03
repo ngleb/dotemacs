6b3b8ac9d6821d542f8db6c33a3a7be36fa5f845
Gleb Nemshilov <gleb@fastmail.com>
2017-02-03 19:56:22 +0700

    Remove unneded keymaps from swiper/ivy/cousel config

--- init.el
+++ init.el
@@ -163,12 +163,6 @@
   (global-set-key (kbd "M-x") 'counsel-M-x)
   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
   (global-set-key (kbd "C-c C-r") 'ivy-resume)
-  ;; (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-  ;; (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-  ;; (global-set-key (kbd "<f1> l") 'counsel-load-library)
-  ;; (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-  ;; (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-
   (define-key read-expression-map (kbd "C-r") 'counsel-expression-history))
 
 (use-package company
