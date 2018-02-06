f125cda03b3d95f2f1ce6130c717d15c9c328cb3
Gleb Nemshilov <gleb@fastmail.com>
2018-02-06 20:21:55 +0700

    enforce utf-8

--- init.el
+++ init.el
@@ -118,10 +118,11 @@
 (setq ad-redefinition-action 'accept)
 
 ;; MULE & encoding setup
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-language-environment "UTF-8")
-(prefer-coding-system 'utf-8)
 (setq default-input-method "russian-computer")
 
 ;; Stop creating backub and autosave files
