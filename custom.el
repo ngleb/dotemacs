0fda66a71990021d73c99d9e948e6028ea3dff36
Gleb Nemshilov <gleb@fastmail.com>
2018-04-10 00:11:50 +0700

    clean-up

--- custom.el
+++ custom.el
@@ -1,11 +1,3 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (smex smart-mode-line smartparens web-mode elpy company zenburn-theme yasnippet w32-browser use-package smooth-scrolling s pyvenv org-plus-contrib olivetti nlinum markdown-mode magit leuven-theme ledger-mode langtool hydra highlight-indentation flyspell-popup flycheck-ledger find-file-in-project dired+ deft counsel))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
