7efb232e8e209ebda07f2025482200f541f867f7
Gleb Nemshilov <gleb@fastmail.com>
2018-05-30 20:54:32 +0700

    emacs-26 release, updates

--- custom.el
+++ custom.el
@@ -4,3 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(whitespace-tab ((t (:foreground "gray40" :background "#424242")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (zenburn-theme web-mode w32-browser use-package smooth-scrolling smex smartparens smart-mode-line org-plus-contrib olivetti nlinum markdown-mode magit leuven-theme ledger-mode langtool hydra flyspell-popup flycheck-ledger elpy dockerfile-mode docker-compose-mode dired+ deft counsel avy))))
