1e2ec7a05f39ca66379075c821dd8d8f376f840b
Gleb Nemshilov <gleb@fastmail.com>
2018-08-19 18:38:23 +0700

    changes

--- init-ledger.el
+++ init-ledger.el
@@ -19,6 +19,7 @@
           ("reg" "%(binary) -f %(ledger-file) --strict -w reg")
           ("payee" "%(binary) -f %(ledger-file) --strict -w reg @%(payee)")
           ("account" "%(binary) -f %(ledger-file) --strict -w reg %(account)")
+          ("account45" "%(binary) -f %(ledger-file) --strict -w -d \"d>=[last 45 days]\" reg %(account)")
           ("bal-assets" "%(binary) -f %(ledger-file) --strict -w bal assets and not Foreign and not Reim")
           ("bal-assets-full" "%(binary) -f %(ledger-file) --strict -w bal assets")
           ("exp-this-month" "%(binary) -f %(ledger-file) --aux-date --strict -w -p \"this month\" bal expenses")
@@ -28,14 +29,7 @@
           ("bu-this-month" "%(binary) -f %(ledger-file) --aux-date --strict -w -p \"this month\" bal --unbudgeted ^exp")
           ("bb-this-month" "%(binary) -f %(ledger-file) --aux-date --strict -w -p \"this month\" bal --budget ^exp --invert")
           ("bu-last-month" "%(binary) -f %(ledger-file) --aux-date --strict -w -p \"last month\" bal --unbudgeted ^exp")
-          ("bb-last-month" "%(binary) -f %(ledger-file) --aux-date --strict -w -p \"last month\" bal --budget ^exp --invert")
-          ("reg-01" "%(binary) -f %(ledger-file) --strict -w -d \"d>=[last 45 days]\" reg assets:cash")
-          ("reg-02" "%(binary) -f %(ledger-file) --strict -w -d \"d>=[last 45 days]\" reg assets:checking:an")
-          ("reg-03" "%(binary) -f %(ledger-file) --strict -w -d \"d>=[last 45 days]\" reg assets:checking:g")
-          ("reg-04" "%(binary) -f %(ledger-file) --strict -w -d \"d>=[last 45 days]\" reg assets:checking:v")
-          ("reg-05" "%(binary) -f %(ledger-file) --strict -w -d \"d>=[last 45 days]\" reg assets:checking:av")
-          ("reg-06" "%(binary) -f %(ledger-file) --strict -w -d \"d>=[last 45 days]\" reg assets:checking:tk")
-          ("reg-07" "%(binary) -f %(ledger-file) --strict -w -d \"d>=[last 45 days]\" reg assets:checking:vt"))))
+          ("bb-last-month" "%(binary) -f %(ledger-file) --aux-date --strict -w -p \"last month\" bal --budget ^exp --invert"))))
 
   (defun my-ledger-mode-hook ()
     (flycheck-mode 1)
@@ -71,10 +65,10 @@ _1_: Assets         _u_: This month  _o_: This month
 _2_: Assets (full)  _i_: Last month  _p_: Last month
 
 Expenses:           Register:
-_3_: This month     _c_: Cash        _m_: AV
-_4_: Last month     _a_: A           _t_: T
-_5_: This week      _g_: G           _h_: VT
-_6_: Last week      _v_: V
+_3_: This month     _r_: Register report for account
+_4_: Last month
+_5_: This week
+_6_: Last week
 
 _q_ quit"
     ("q" nil)
@@ -92,13 +86,7 @@ _q_ quit"
     ("o" (gn/ledger-report "bu-this-month"))
     ("p" (gn/ledger-report "bu-last-month"))
 
-    ("c" (gn/ledger-report "reg-01"))
-    ("a" (gn/ledger-report "reg-02"))
-    ("g" (gn/ledger-report "reg-03"))
-    ("v" (gn/ledger-report "reg-04"))
-    ("m" (gn/ledger-report "reg-05"))
-    ("t" (gn/ledger-report "reg-06"))
-    ("h" (gn/ledger-report "reg-07"))))
+    ("r" (gn/ledger-report "account45"))))
 
 (use-package flycheck-ledger
   :after flycheck)
