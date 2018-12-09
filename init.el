54438f51a6ac2e5bb438ed30296e811fb3c4cadc
Gleb Nemshilov <gleb@fastmail.com>
2018-12-09 23:58:32 +0700

    changes

--- init.el
+++ init.el
@@ -547,7 +547,7 @@
         '(("default"
            ("Dired" (mode . dired-mode))
            ("Planner"
-            (or (filename . "\\(todo\\|refile\\|reading\\|someday\\|archive\*\\).org")
+            (or (filename . "\\(todo\\|refile\\|reading\\|mobile\\|someday\\|archive\*\\).org")
                 (mode . org-agenda-mode)
                 (name . "^\\*Calendar\\*$")
                 (name . "^diary$")))
