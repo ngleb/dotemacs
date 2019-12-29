7a8c015e57cb567b7cb957d6afaf5237dba95b15
Gleb Nemshilov <gleb@fastmail.com>
2019-12-30 03:15:38 +0700

    changes

--- init.el
+++ init.el
@@ -509,10 +509,10 @@
   :diminish (global-whitespace-mode
              whitespace-mode
              whitespace-newline-mode)
-  :init
-  (dolist (hook '(prog-mode-hook text-mode-hook conf-mode-hook))
-    (add-hook hook #'whitespace-mode))
-  (add-hook 'before-save-hook #'whitespace-cleanup)
+  ;; :init
+  ;; (dolist (hook '(prog-mode-hook text-mode-hook conf-mode-hook))
+  ;;   (add-hook hook #'whitespace-mode))
+  ;; (add-hook 'before-save-hook #'whitespace-cleanup)
   :config
   (setq whitespace-line-column 80) ;; limit line length
   (setq whitespace-style '(face tabs tab-mark trailing)))
