62242581d5f0909399693263761c9d53542b042a
Gleb Nemshilov <gleb@fastmail.com>
2020-01-02 08:32:27 +0700

    changes

--- init.el
+++ init.el
@@ -12,6 +12,10 @@
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system
+ (if (eq system-type 'windows-nt)
+     'utf-16-le  ;; https://rufflewind.com/2014-07-20/pasting-unicode-in-emacs-on-windows
+   'utf-8))
 (prefer-coding-system 'utf-8)
 
 (require 'package)
