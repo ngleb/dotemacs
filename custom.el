b1d6a5bfce1f924d771fa1149c58a5a3aa3e77e1
Gleb Nemshilov <gleb@fastmail.com>
2020-05-11 12:30:22 +0700

    Update selected packages

--- custom.el
+++ custom.el
@@ -4,7 +4,48 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zenburn-theme which-key web-mode w32-browser use-package speed-type sokoban smex smartparens ox-pandoc ox-clip org-plus-contrib olivetti nginx-mode markdown-mode magit ledger-mode langtool js2-mode hydra flyspell-popup flycheck-ledger find-file-in-project elpy elfeed dockerfile-mode docker-compose-mode diminish deft counsel avy)))
+   '(ag
+     avy
+     company
+     counsel
+     csv-mode
+     deft
+     diminish
+     docker-compose-mode
+     dockerfile-mode
+     elfeed
+     elpy
+     find-file-in-project
+     flycheck
+     flycheck-ledger
+     flyspell-popup
+     helm
+     helm-descbinds
+     helm-org
+     helm-swoop
+     hydra
+     js2-mode
+     langtool
+     ledger-mode
+     magit
+     markdown-mode
+     nginx-mode
+     olivetti
+     org-plus-contrib
+     ox-clip
+     ox-pandoc
+     pyvenv
+     smartparens
+     smex
+     sokoban
+     swiper
+     treemacs
+     use-package
+     w32-browser
+     web-mode
+     which-key
+     yasnippet
+     zenburn-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
